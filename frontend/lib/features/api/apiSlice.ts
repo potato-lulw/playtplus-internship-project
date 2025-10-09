6688c5893e4f57a5279b477d5a73bda43e0121ca
potato-lulw <om.patil17072@gmail.com>
2025-10-09 19:17:31 +0530

    maybe?

--- frontend/lib/features/api/apiSlice.ts
+++ frontend/lib/features/api/apiSlice.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl =  process.env.NEXT_PUBLIC_API_URL + '/api/v1'
+const baseUrl = process.env.NEXT_PUBLIC_API_URL + '/api/v1'
+
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl }),
-  endpoints: (builder) => (
-    {}),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl,
+    credentials: 'include',
+  }),
+  endpoints: (builder) => ({}),
 })
-
-
