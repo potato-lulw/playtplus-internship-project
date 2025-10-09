1f890b7e44a4dac3cb05ebc994ec653627ef4afe
potato-lulw <om.patil17072@gmail.com>
2025-10-09 18:12:31 +0530

    env name changes

--- frontend/lib/features/api/apiSlice.ts
+++ frontend/lib/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl =  process.env.API_URL ||  'http://localhost:8800' + '/api/v1'
+const baseUrl =  process.env.NEXT_PUBLIC_API_URL  ||  'http://localhost:8800' + '/api/v1'
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl }),
